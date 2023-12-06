65f18998d7043d930ebef50cf649c6dfc9778e4f
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-06 13:12:08 +0100

    Add project files.

---
+++ microsoftLearnExercise6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+
+namespace microsoftLearnExercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+        
+            string[] orderIDs = orderStream.Split(',');
+            Array.Sort(orderIDs);
+
+            foreach (string orderID in orderIDs)
+            {
+                if (orderID.Length != 4)
+                    Console.WriteLine($"{orderID} \t- Error");
+                else
+                    Console.WriteLine($"{orderID}");
+
+            }
+        }
+    }
+}
